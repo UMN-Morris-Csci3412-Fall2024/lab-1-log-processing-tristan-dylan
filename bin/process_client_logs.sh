da08ef907a3090fc29860491def148b0ff5ee030
Tristan Kalvoda <kalvo007@morris.umn.edu>
2024-09-12 12:15:16 -0400

    finished process_client_logs.sh

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -4,3 +4,11 @@
 cd $1
 
 #awk '/Failed password for invalid user/ {print $1, $2, $3, $11, $13}' var/log/* > failed_login_data.txt
+cat var/log/* | awk '/Failed password for invalid user/ {
+    split($3, time, ":");
+    print $1, $2, time[1], $11, $13
+}
+/Failed password for / && !/invalid user/ {
+    split($3, time, ":");
+    print $1, $2, time[1], $9, $11
+}' > failed_login_data.txt
