e1d5b42bd2f3de049df7693cc9164bf63033c058
Tristan Kalvoda <kalvo007@morris.umn.edu>
2024-09-17 13:32:04 -0400

    Fixed create_country_dist.sh
    The problem was the wrap_contents.sh was being called incorrectly

--- bin/create_country_dist.sh
+++ bin/create_country_dist.sh
@@ -83,6 +83,7 @@
 
 # Move to specified directory
 cd "$1"
+echo "Moved to $(pwd)"
 
 # Temporary file to store all IP addresses
 tempfile=$(mktemp)
@@ -97,20 +98,23 @@ done
 # Sort the extracted IP addresses
 sortedtempfile=$(mktemp)
 sort "$tempfile" > "$sortedtempfile"
+echo "sorted and extracted IP adresses"
 
 # Join IP addresses with country map
 joinedtempfile=$(mktemp)
 join -1 1 -2 1 "$sortedtempfile" <(sort ../etc/country_IP_map.txt) > "$joinedtempfile"
+echo "joined IP addresses with country_IP_map"
 
 # Extract country codes and count occurrences
 countrytempfile=$(mktemp)
 awk '{print $2}' "$joinedtempfile" | sort | uniq -c > "$countrytempfile"
+echo "extracted country codes and counted occurences"
 
 # Format the data for the Geo Chart
 awk '{print "data.addRow([\x27" $2 "\x27, " $1 "]);"}' "$countrytempfile" > "$tempfile"
+echo "printed:" $tempfile
 
 # Wrap the data section with header and footer
-../bin/wrap_contents.sh "$tempfile" ../html_components/country_dist_header.html ../html_components/country_dist_footer.html > country_dist.html
-
+../bin/wrap_contents.sh "$tempfile" ../html_components/country_dist country_dist.html
 # Clean up temporary files
 rm "$tempfile" "$sortedtempfile" "$joinedtempfile" "$countrytempfile"
