cdc913b109f2d1b81eed3709b39bbcca90a880ba
Tristan Kalvoda <kalvo007@morris.umn.edu>
2024-09-19 11:20:46 -0400

    changed create_username_dist to use wrap_contents

--- bin/create_username_dist.sh
+++ bin/create_username_dist.sh
@@ -3,9 +3,6 @@
 # Move to the specified directory
 cd $1
 
-# Initialize the output file
-cp ../html_components/username_dist_header.html username_dist.html
-
 # Temporary file to store all usernames
 temp_file=$(mktemp)
 
@@ -19,10 +16,11 @@ done
 # Sort and count occurrences of each username
 sort "$temp_file" | uniq -c | awk '{
     printf "data.addRow([\x27%s\x27, %d]);\n", $2, $1
-}' >> username_dist.html
+}' >> username_dist_data.txt
 
-# Append the footer
-cat ../html_components/username_dist_footer.html >> username_dist.html
+
+# Wrap the data section with header and footer
+../bin/wrap_contents.sh username_dist_data.txt ../html_components/username_dist username_dist.html
 
 # Clean up temporary file
-rm "$temp_file"
+rm "$temp_file" username_dist_data.txt
