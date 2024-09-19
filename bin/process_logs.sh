879b0c973d49e41edaeb912221cf5124f859ae46
Tristan Kalvoda <kalvo007@morris.umn.edu>
2024-09-19 11:37:49 -0400

    Added echo commands to help with debugging

--- bin/process_logs.sh
+++ bin/process_logs.sh
@@ -4,24 +4,35 @@
 scratch=$(mktemp -d)
 echo "Created scratch directory: $scratch"
 
-for file in "$1"/*.tgz; do
+for file in "$@"; do
     machineName=$(basename "$file" _secure.tgz)
     machineNameDir="$scratch/$machineName"
+    echo "Processing $machineName"
     mkdir -p "$machineNameDir"
+    echo "Created directory: $machineNameDir"
     tar -xzf "$file" -C "$machineNameDir"
-    bin/process_client_logs.sh "$machineNameDir"
+    echo "Unzipped $file to $machineNameDir"
+    ./bin/process_client_logs.sh "$machineNameDir"
 done
+echo  $(pwd)
+echo "Processing logs complete"
 
 #Calls create_username_dist.sh, create_hours_dist.sh, and create_country_dist.sh on the scratch directory
-bin/create_username_dist.sh "$scratch"
-bin/create_hours_dist.sh "$scratch"
-bin/create_country_dist.sh "$scratch"
+./bin/create_username_dist.sh "$scratch"
+echo "Created username distribution"
+./bin/create_hours_dist.sh "$scratch"
+echo "Created hours distribution"
+./bin/create_country_dist.sh "$scratch"
+echo "Created country distribution"
 
 #Assembles the report
-bin/assemble_report.sh "$scratch"
+./bin/assemble_report.sh "$scratch"
+echo "Assembled report"
 
 #Moves the report to the current directory
 mv "$scratch"/failed_login_summary.html "$2"
+echo "Moved report to $2"
 
 #Removes the scratch directory
 rm -r "$scratch"
+echo "Removed scratch directory: $scratch"
