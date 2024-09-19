a84117e65209bb97d85cb34165473700e32b8a96
Tristan Kalvoda <kalvo007@morris.umn.edu>
2024-09-19 11:20:01 -0400

    changed create_hours_dist to use wrap
    Co-authored-by: Dylan Packer <packe089@users.noreply.github.com>

--- bin/create_hours_dist.sh
+++ bin/create_hours_dist.sh
@@ -3,9 +3,6 @@
 # Move to the specified directory
 cd $1
 
-# Initialize the output file
-cp ../html_components/hours_dist_header.html hours_dist.html
-
 # Temporary file to store all times
 temp_file=$(mktemp)
 
@@ -19,10 +16,10 @@ done
 # Sort and count occurrences of each username
 sort "$temp_file" | uniq -c | awk '{
     printf "data.addRow([\x27%s\x27, %d]);\n", $2, $1
-}' >> hours_dist.html
+}' >> hours_dist_data.txt
 
-# Append the footer
-cat ../html_components/hours_dist_footer.html >> hours_dist.html
+# Wrap the data section with header and footer
+../bin/wrap_contents.sh hours_dist_data.txt ../html_components/hours_dist hours_dist.html
 
 # Clean up temporary file
-rm "$temp_file"
+rm "$temp_file" hours_dist_data.txt
